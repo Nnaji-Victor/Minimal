9f1c38faa82c6998236d0ab6debdc8d33d3e45ab
Victor Nnaji <nnajivictor0@gmail.com>
2020-08-22 23:15:08 +0100

    build utility class

--- Minimal.Utility/SD.cs
+++ Minimal.Utility/SD.cs
@@ -6,5 +6,9 @@ namespace Minimal.Utility
 {
     public static class SD
     {
+        public static string Truncate(this string value, int maxChars)
+        {
+            return value.Length <= maxChars ? value : value.Substring(0, maxChars) + "...";
+        }
     }
 }
